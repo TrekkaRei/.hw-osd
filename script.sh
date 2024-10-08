e04e7c5e106a78ed07b301d5bd642af787a2665e
TrekkaRei <TrekkaRei@gmail.com>
2024-10-09 00:57:51 +0200

    update

--- script.sh
+++ script.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
-
-cputemp=$(sensors | grep -w "Tctl" | cut -c 16-21 | cut -c 1-2)
-gputemp=$(nvidia-smi | grep -w Default | cut -c 9-12 | cut -c 1-2)
-cpuusage=$(top -i -bn 1 | grep '%Cpu' | awk '{print 100-$8}')
+# GPU variables
 gpuusage=$(nvidia-smi -q | grep -w "Gpu" | awk '{print $3}')
-memusage=$(fastfetch --logo none | grep RAM | cut -c 7-11)
+gputemp=$(nvidia-smi | grep -w Default | cut -c 9-12 | cut -c 1-2)
+
+# CPU variables
+cpuusage=$(top -i -bn 1 | grep '%Cpu' | awk '{print 100-$8}')
+cputemp=$(sensors | grep -w "Tctl" | cut -c 16-21 | cut -c 1-2)
+
+# Memory variable
+memusage=$(fastfetch --logo none | grep RAM | awk '{print $2}')
+
+# other variables
 see=$(echo C)
 giga=$(echo GB)
 
 echo " GPU:[$gpuusage% $gputemp$see]   CPU:[$cpuusage% $cputemp$see]   MEM:[$memusage$giga]"
-
