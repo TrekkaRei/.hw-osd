2917cbfd3513b22bc2e220dee4bc88516402f946
TrekkaRei <TrekkaRei@gmail.com>
2024-10-10 01:15:48 +0200

    fixed things

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
 # GPU variables
 gpuusage=$(nvidia-smi -q | grep -w "Gpu" | awk '{print $3}')
-gputemp=$(inxi -s | grep temp: | cut -d' ' -f8)
+gputemp=$(inxi -s | grep temp: | cut -d' ' -f11)
 
 # CPU variables
 cpuusage=$(top -i -bn 1 | grep '%Cpu' | awk '{print 100-$8}')
-cputemp=$(inxi -s | grep ures: | cut -d' ' -f6)
+cputemp=$(inxi -s | grep ures: | cut -d' ' -f6 | cut -c 1-2)
 
 # Memory variable
-memusage=$(inxi -m | grep used | cut -d' ' -f8,9)
+memusage=$(inxi -m | grep used | cut -d' ' -f6,7)
 
 # other variables
 see=$(echo C)
--- watchosd.sh
+++ watchosd.sh
@@ -1,5 +1,5 @@
 sleep 1
 tput civis
 clear
-resize -s 1 50
+resize -s 1 47
 watch -t -n 0.5 ~/.hw-osd/script.sh
