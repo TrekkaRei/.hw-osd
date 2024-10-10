960e84a252bb60b699281d27b69a09bfb61bce0d
TrekkaRei <TrekkaRei@gmail.com>
2024-10-10 04:04:42 +0200

    update

--- Main.sh
+++ Main.sh
@@ -1,4 +1,4 @@
 # geometry is based off of 2560x1440 screen
 
-gnome-terminal --geometry=47x1+2090+15 -t "hardware osd" -- ~/.hw-osd/watchosd.sh
+gnome-terminal --geometry=44x1+2158+15 -t "hardware osd" -- ~/.hw-osd/watchosd.sh
 xprop -name 'hardware osd' -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x2, 0x0, 0x0"
--- script.sh
+++ script.sh
@@ -7,9 +7,10 @@ cpuusage=$(top -bn 1 | grep %Cpu | awk '{print 101-$8}')
 cputemp=$(inxi -s | grep ures: | cut -d' ' -f6 | cut -c 1-2)
 
 # Memory variable
-memusage=$(inxi -m | grep used | cut -d' ' -f6,7)
+memusage=$(inxi -c | grep Mem | cut -d':' -f2 | cut -c 2-6 | cut -d'.' -f1)
 
 # other variables
-see=$(echo C)
+see=$(echo °C)
+mega=$(echo MB)
 
-echo " GPU:[$gpuusage% $gputemp$see]  CPU:[$cpuusage% $cputemp$see]  MEM:[$memusage]"
+echo "GPU:[$gpuusage% $gputemp$see] CPU:[$cpuusage% $cputemp$see] RAM:[$memusage $mega]"
